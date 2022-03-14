74d7eccf7c1930439e63a63d3c79d3f47bd42ff7
Jose Vargas <arnoldovargas1981@gmail.com>
2022-03-14 16:59:06 -0300

    Terminado

---
+++ conf/dbConnection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'localhost', // Replace with your host name
+    user: 'root',      // Replace with your database username
+    password: '#Root2022',      // Replace with your database password
+    database: 'mysql_crud_rest_api' // // Replace with your database Name
+});
+
+conn.connect(function (err) {
+    if (err) throw err;
+    console.log('Database is connected successfully !');
+});
+module.exports = conn;
